795685b78629a7f82eca054ee1d6aed5aa75140a
Vamsi Krishna Bandaru <008771055@coyote.csusb.edu>
2024-09-26 03:13:33 -0700

    adjusted gaps between like and dislike button

--- app.py
+++ app.py
@@ -93,12 +93,12 @@ def main():
                 handle_feedback(len(st.session_state.messages) - 1, "dislike")
 
 
-if _name_ == "main":
+if __name__ == "__main__":
     # If streamlit instance is running
     if os.environ.get("STREAMLIT_RUNNING") == "1":
         main()
     else:
         os.environ["STREAMLIT_RUNNING"] = "1"  # Set the environment variable to indicate Streamlit is running
-        subprocess.Popen(["streamlit", "run", file, "--server.port=5001", "--server.address=0.0.0.0"])
+        subprocess.Popen(["streamlit", "run", __file__, "--server.port=5001", "--server.address=0.0.0.0"])
         subprocess.Popen(["service", "nginx", "start"])
         subprocess.run(["jupyter", "notebook", "--ip=0.0.0.0", "--port=6001", "--no-browser", "--allow-root"])
