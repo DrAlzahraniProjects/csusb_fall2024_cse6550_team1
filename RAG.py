183abaed80ca19b2ad7842b73621bded6cbbfddb
smrchanda877 <008735877@coyote.csusb.edu>
2024-10-18 01:07:53 -0700

    minor count error fix

--- RAG.py
+++ RAG.py
@@ -73,11 +73,11 @@ def query_rag(query):
             break
         else:
             source = i.metadata["source"]
+            count += 1
             if source in sources:
                 continue
             sources.add(source)
             all_sources += f"[Source {count}]({source}), "
-            count += 1
     all_sources = all_sources[:-2]
     response["answer"] += f"\n\nSources: {all_sources}"
     print("Response Generated")
