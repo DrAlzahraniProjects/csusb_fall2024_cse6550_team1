ca049142c5abc02b7023ac8d0cc39cbf094bca1f
itsnotmik <mclement389@gmail.com>
2024-12-01 22:33:39 -0800

    removed langchain_milvus

--- backend/RAG.py
+++ backend/RAG.py
@@ -14,7 +14,6 @@ from dotenv import load_dotenv
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.schema import Document
 from langchain_core.prompts import ChatPromptTemplate
-#from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_community.document_loaders import RecursiveUrlLoader, WebBaseLoader
 from bs4 import BeautifulSoup
 from sentence_transformers import SentenceTransformer
